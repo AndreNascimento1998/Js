94ac78d8b43d62bbe0a8e407605bfada72fa423c
André Joarez <andre.ncardoso@hotmail.com>
2023-01-17 19:00:26 -0300

    finalizado, agora interação com ajax

---
+++ js/filtra.js
@@ -0,0 +1,22 @@
+const campofiltro = document.querySelector('#filtrar-tabela');
+campofiltro.addEventListener('input', function() {
+    const paciente = document.querySelectorAll('.paciente');
+
+    debugger;
+    if(this.value.length > 0){
+        paciente.forEach((vl) => {
+            const tdNome = vl.querySelector('.info-nome');
+            const nome = tdNome.textContent
+            const expressao = new RegExp(this.value, 'i', );
+            if(!expressao.test(nome)){
+                vl.classList.add('invisivel');
+            }else {
+                vl.classList.remove('invisivel');
+            }
+        });
+    }else {
+        paciente.forEach((vl) => {
+            vl.classList.remove('invisivel')
+        })
+    }
+})
